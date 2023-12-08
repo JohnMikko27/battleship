e4be523f5343c9459ea8be76e4afd7ca467f6fb0
Mikko <realmiks27@gmail.com>
2023-12-08 13:57:50 -0800

    Add jest and babel

--- dist/main.js
+++ dist/main.js
@@ -5,9 +5,5 @@ var __webpack_exports__ = {};
   \**********************/
 
 
-
-
-console.log('pop')
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7QUFJQSxrQiIsInNvdXJjZXMiOlsid2VicGFjazovL2JhdHRsZXNoaXAvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiXG5cblxuXG5jb25zb2xlLmxvZygncG9wJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
