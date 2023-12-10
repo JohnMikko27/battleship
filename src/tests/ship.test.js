cc1fae27fdfc94f9a4d6d44ea59feff393267430
Mikko <realmiks27@gmail.com>
2023-12-10 08:00:04 -0800

    Add ship tests

---
+++ src/tests/ship.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import Ship from "../modules/ship";
+
+let ship;
+let ship2;
+
+beforeEach(() => {
+  ship = Ship(4);
+  ship2 = Ship(1);
+});
+
+test("get number of times hit", () => {
+  ship.hit();
+  expect(ship.getNumOfHits()).toBe(1);
+});
+
+test("check if ship isSunk", () => {
+  ship2.hit();
+  expect(ship2.isSunk()).toBeTruthy();
+});
+
+test("check if ship is not sunk", () => {
+  expect(ship.isSunk()).toBeFalsy();
+});
+
